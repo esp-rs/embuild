8e34161d412808eb66bc5585a7c3afb84d514bad
N3xed <dominik.gschwind99@gmail.com>
2021-09-08 17:44:17 +0200

    Use reponse file on windows and when using gcc (#15)
    This is to circumvent the command-line length limitation on windows.
    
    Export `shlex::join` as `join_unix_args`
    Export `shlex::quote` as `quote_unix_arg`
    Change `LinkArgsBuilder::build` to return a `Result<LinkArgs>`
    Add `cargo::out_dir`

--- src/cli/separate_args.rs
+++ src/cli/separate_args.rs
@@ -155,6 +155,8 @@ impl<'a> Iterator for WindowsCommandArgs<'a> {
 }
 
 pub use shlex::Shlex as UnixCommandArgs;
+pub use shlex::join as join_unix_args;
+pub use shlex::quote as quote_unix_arg;
 
 #[cfg(windows)]
 pub type NativeCommandArgs<'a> = WindowsCommandArgs<'a>;
