380ac349ee832c19458aec0079d42cab999d62d4
Hailey Somerville <hailey@hailey.lol>
2023-09-15 13:58:35 +1000

    add SourceTree::Plain variant

--- src/espidf.rs
+++ src/espidf.rs
@@ -354,12 +354,14 @@ pub struct EspIdf {
 #[derive(Debug, Clone)]
 pub enum SourceTree {
     Git(git::Repository),
+    Plain(PathBuf),
 }
 
 impl SourceTree {
     pub fn path(&self) -> &Path {
-        match tree {
+        match self {
             SourceTree::Git(repo) => repo.worktree(),
+            SourceTree::Plain(path) => path,
         }
     }
 }
@@ -371,11 +373,9 @@ impl EspIdf {
         let idf_path = env::var_os(IDF_PATH_VAR).ok_or_else(|| {
             FromEnvError::NoRepo(anyhow!("environment variable `{IDF_PATH_VAR}` not found"))
         })?;
-        let tree = match git::Repository::open(idf_path) {
+        let tree = match git::Repository::open(&idf_path) {
             Ok(repo) => SourceTree::Git(repo),
-            Err(e) => {
-                return Err(FromEnvError::NoRepo(e));
-            }
+            Err(_) => SourceTree::Plain(PathBuf::from(idf_path)),
         };
 
         let path_var = env::var_os("PATH").unwrap_or_default();
